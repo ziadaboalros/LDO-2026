4a8d3f11f080081b742a382f062c4e89d2236659
ziadaboalros <aboalrosziad@gmail.com>
2026-09-12 01:39:26 +0300

    Rename module tt_um_example to LDO-2026

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module LDO-2026 (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
