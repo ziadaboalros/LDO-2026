d2e9b3b964b05d3aa7e0684a51f778a441b01f70
ziadaboalros <aboalrosziad@gmail.com>
2026-09-12 01:47:41 +0300

    Rename module from tt_um_LDO-2026 to tt_um_LDO_2026

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_LDO-2026 (
+module tt_um_LDO_2026 (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
