43689fff4bdcc9c99cf6eb3aa88eefc866aaa620
ziadaboalros <aboalrosziad@gmail.com>
2026-09-13 22:19:37 +0300

    Change ua from 1-bit to 8-bit in project.v
    Updated the width of the analog pins from 1 to 8 bits.

--- src/project.v
+++ src/project.v
@@ -14,7 +14,7 @@ module tt_um_LDO_2026 (
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    inout  wire [0] ua,       // Analog pins, only ua[5:0] can be used
+    inout  wire [7:0] ua,       // Analog pins, only ua[5:0] can be used
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
